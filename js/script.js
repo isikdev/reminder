57b17f735ffedf83194a5a701871f51714b2dea2
iskandar <isikjandev@gmail.com>
2022-10-02 12:54:00 +0500

    push

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button")
+
+
+button.addEventListener("click", () => {
+    Notification.requestPermission().then(perm => {
+        if (perm === "granted ") {
+            new Notification("Example notification ")
+        }
+    })
+})
